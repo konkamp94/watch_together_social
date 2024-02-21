0ed54ea4f05245eb3f0d1ac66f36a3fbc1f5a9a6
Konstantinos Kampourelis <konkamp94@gmail.com>
2024-02-21 21:02:40 +0200

    postgres with typeorm integration

--- watch_together_be/src/app.module.ts
+++ watch_together_be/src/app.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { Logger } from '@nestjs/common';
+
+const ENV = process.env.NODE_ENV || 'development';
+const envFilePath = `config/.${ENV}.env`;
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: [envFilePath] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: parseInt(configService.get('DATABASE_PORT')),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(configService: ConfigService) { }
+}
